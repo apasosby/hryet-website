97f841628bb0db745a6c6ad796501d80247d5a9a
apasosby <henrrymoysesg@gmail.com>
2026-06-29 16:29:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -29,8 +29,54 @@ function guardarPerfil() {
     alert("¡Identidad Digital Actualizada!");
     window.location.href = "perfil.html";
 }
-
 // Ejecutar contador si estamos en perfil.html
 if (window.location.pathname.includes('perfil.html')) {
     actualizarContador();
 }
+const canvas = document.getElementById('particles');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 2 + 0.1;
+        this.speedX = Math.random() * 1 - 0.5;
+        this.speedY = Math.random() * 1 - 0.5;
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    draw() {
+        ctx.fillStyle = 'rgba(188, 19, 254, 0.5)';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function init() {
+    for (let i = 0; i < 150; i++) {
+        particlesArray.push(new Particle());
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
+
+// Scroll Reveal para animaciones suaves
+ScrollReveal().reveal('.glass', { delay: 200, distance: '50px', origin: 'bottom' });
