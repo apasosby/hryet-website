2cb8311a1c0aab224ed3812a97255a058b2a3c71
apasosby <henrrymoysesg@gmail.com>
2026-06-30 04:27:54 -0400

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,37 @@
+class User {
 
+    constructor(name, email, password) {
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+        this.profilePhoto = "";
+
+        this.bio = "";
+
+        this.socials = {
+
+            instagram: "",
+
+            tiktok: "",
+
+            facebook: "",
+
+            x: ""
+
+        };
+
+        this.qrCode = "";
+
+        this.likes = 0;
+
+        this.scans = 0;
+
+        this.createdAt = new Date();
+
+    }
+
+}
+
+module.exports = User;
