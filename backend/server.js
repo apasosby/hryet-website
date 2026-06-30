e8fd6cd8afba261b05287bf693a0b7fd934087ea
apasosby <henrrymoysesg@gmail.com>
2026-06-30 04:24:32 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+const express = require("express");
 
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Servidor HRYET funcionando 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
