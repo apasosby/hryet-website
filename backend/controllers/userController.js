427bf1b5f968768e7279ec0c310a17cdb1374a18
apasosby <henrrymoysesg@gmail.com>
2026-06-30 04:41:55 -0400

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,18 @@
+const UserService = require("../services/userService");
 
+exports.getProfile = (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Perfil obtenido"
+    });
+
+};
+
+exports.updateProfile = (req, res) => {
+
+    const user = UserService.createUser(req.body);
+
+    res.json(user);
+
+};
