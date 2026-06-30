7a41ab8e7fb1589267db23294b5b7adfd4193b90
apasosby <henrrymoysesg@gmail.com>
2026-06-30 04:48:21 -0400

    Update firebase.js

--- database/firebase.js
+++ database/firebase.js
@@ -1,17 +1,14 @@
+const { initializeApp } = require("firebase/app");
+
 const firebaseConfig = {
-
-    apiKey: "TU_API_KEY",
-
-    authDomain: "TU_PROYECTO.firebaseapp.com",
-
-    projectId: "TU_PROYECTO",
-
-    storageBucket: "TU_PROYECTO.appspot.com",
-
-    messagingSenderId: "000000000000",
-
-    appId: "TU_APP_ID"
-
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_MESSAGING_ID",
+  appId: "TU_APP_ID"
 };
 
-module.exports = firebaseConfig;
+const app = initializeApp(firebaseConfig);
+
+module.exports = app;
