b89090282af5d3fe851d2e75977e65c1b9676ee9
apasosby <henrrymoysesg@gmail.com>
2026-06-30 04:13:45 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("HRYET iniciado correctamente.");
+
+    console.log("HRYET iniciado");
+
+    const botones = document.querySelectorAll("button");
+
+    botones.forEach((boton) => {
+
+        boton.addEventListener("mouseenter", () => {
+            boton.style.transform = "scale(1.05)";
+        });
+
+        boton.addEventListener("mouseleave", () => {
+            boton.style.transform = "scale(1)";
+        });
+
+    });
+
 });
