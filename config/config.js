23c5def938669979cd796d2645f9a7333a118ebc
apasosby <henrrymoysesg@gmail.com>
2026-06-30 05:21:03 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    API_URL: "http://localhost:3000/api",
     APP_NAME: "HRYET",
-    VERSION: "1.0.0"
+    VERSION: "1.0.0",
+    API_URL: "http://localhost:3000/api"
 };
 
 export default CONFIG;
