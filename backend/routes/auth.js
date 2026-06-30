dec8915f75c003e83f1614480c5177c86dfe5883
apasosby <henrrymoysesg@gmail.com>
2026-06-30 04:25:56 -0400

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Usuario registrado correctamente"
+    });
+
+});
+
+router.post("/login", (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Inicio de sesión correcto"
+    });
+
+});
+
+module.exports = router;
 
