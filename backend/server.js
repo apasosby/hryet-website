212dd961fbbe551956ef8ebfb42826caa899427c
apasosby <henrrymoysesg@gmail.com>
2026-06-30 04:38:54 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
-const app = express();
 
-const authRoutes = require("./routes/auth");
+const app = express();
 
 app.use(express.json());
 
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+const productRoutes = require("./routes/products");
+const orderRoutes = require("./routes/orders");
+const adminRoutes = require("./routes/admin");
+const qrRoutes = require("./routes/qr");
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/qr", qrRoutes);
 
 app.get("/", (req, res) => {
-    res.send("🚀 HRYET API funcionando");
+    res.send("🚀 HRYET API ONLINE");
 });
 
 const PORT = process.env.PORT || 3000;
