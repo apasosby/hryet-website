b006f22fb5c9f2ff7426516837c476be2b3f9366
apasosby <henrrymoysesg@gmail.com>
2026-06-30 04:26:59 -0400

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,28 @@
+exports.register = (req, res) => {
 
+    const { name, email, password } = req.body;
+
+    res.status(201).json({
+        success: true,
+        message: "Usuario registrado",
+        user: {
+            name,
+            email
+        }
+    });
+
+};
+
+exports.login = (req, res) => {
+
+    const { email } = req.body;
+
+    res.status(200).json({
+        success: true,
+        message: "Inicio de sesión exitoso",
+        user: {
+            email
+        }
+    });
+
+};
